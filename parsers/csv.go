ababf17919bfbdda141a07abcbfa8c211ecd00e8
Casey Clayton <claytoncasey01@gmail.com>
2024-06-17 18:10:26 -0400

    Move helpers to base parser file

--- parsers/csv.go
+++ parsers/csv.go
@@ -44,8 +44,8 @@ func (p CsvParser) Parse(file multipart.File) (any, error) {
 	recipe := models.Recipe{
 		Name:          records[0][0],
 		TotalPrepTime: &records[0][2],
-		TotalCalories: parseUint(records[0][3]),
-		Difficulty:    parseUint(records[0][4]),
+		TotalCalories: ParseUint(records[0][3]),
+		Difficulty:    ParseUint(records[0][4]),
 	}
 
 	// Parse ingredients and directions from the rest of the rows
@@ -59,9 +59,9 @@ func (p CsvParser) Parse(file multipart.File) (any, error) {
 		}
 		// Ingredient Rows
 		if i < 5 {
-			quantity, unit := parseQuantity(record[1])
+			quantity, unit := ParseQuantity(record[1])
 			ingredients = append(ingredients, models.Ingredient{
-				Name:            parseName(record[1], quantity, unit),
+				Name:            ParseName(record[1], quantity, unit),
 				Quantity:        quantity,
 				MeasurementUnit: unit,
 			})
@@ -82,59 +82,4 @@ func (p CsvParser) Parse(file multipart.File) (any, error) {
 	recipe.Directions = directions
 
 	return recipe, nil
-}
-
-func parseUint(s string) *uint {
-	if s == "" {
-		return nil
-	}
-	intVal, err := strconv.Atoi(s)
-	if err != nil {
-		return nil
-	}
-	uintVal := uint(intVal)
-
-	return &uintVal
-}
-
-// Handles parsing out the quantity and measurement unit from the ingredient string
-func parseQuantity(ingredient string) (string, *string) {
-	// Splits the ingredient string into parts based on whitespace
-	parts := strings.Fields(ingredient)
-	if len(parts) < 2 {
-		return "", nil
-	}
-
-	quantity := parts[0]
-	unit := ""
-
-	for _, part := range parts[1:] {
-		for _, q := range commonQuantities {
-			// We found the unit, so we break out of the loop
-			if strings.Contains(part, q) {
-				unit = part
-				break
-			}
-		}
-		// We found a unit, no need to continue searching
-		if unit != "" {
-			break
-		}
-	}
-
-	if unit == "" {
-		return quantity, nil
-	}
-
-	return quantity, &unit
-}
-
-func parseName(ingredient, quantity string, unit *string) string {
-	// Remove the quantity and unit from the ingredient string
-	name := strings.Replace(ingredient, quantity, "", 1)
-	if unit != nil {
-		name = strings.Replace(name, *unit, "", 1)
-	}
-
-	return strings.TrimSpace(name)
 }
